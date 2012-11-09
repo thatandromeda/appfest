814dd4da48205a8e21a196ad5034da4145535526
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-11-09 15:47:16 -0500

    django should be able to find stylesheets and so should non-django devs

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, render_to_response
 from appfest.core.models import *
+from django.conf import settings
 
 def home(request):
-    return render_to_response('askaquestion.html') 
+    return render_to_response('askaquestion.html', {'static': settings.STATIC_URL}) 
 
 def open(request):
     if Question.objects.count() < 5:
@@ -11,10 +12,12 @@ def open(request):
         questions = Question.objects.all()[0:5]
     return render(request, 'questionpage.html', {
         'questions': questions,
+        'static': settings.STATIC_URL,
     }) 
     
 def question(request, question_id):
 
     return render(request, 'question.html', {
         'context': context,
+        'static': settings.STATIC_URL,
     }) 
