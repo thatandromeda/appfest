077a6043be8fb1e9d01a4daddd50fae618662bc2
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-11-09 15:04:12 -0500

    adding context for question page

--- core/views.py
+++ core/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render, render_to_response
+from appfest.core.models import *
 
 def home(request):
     return render_to_response('askaquestion.html') 
 
 def open(request):
+    if Question.objects.count() < 5:
+        questions = Question.objects.all()
+    else:
+        questions = Question.objects.all()[0:5]
     return render(request, 'questionpage.html', {
-        'context': context,
+        'questions': questions,
     }) 
     
 def question(request, question_id):
+
     return render(request, 'question.html', {
         'context': context,
     }) 
